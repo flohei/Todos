4404570711b6012a7ff5d4609511749c62de17bc
Florian Heiber <flo@flohei.de>
2020-10-16 20:08:10 +0200

    Split files while keeping empty lines

--- Sources/Todos/File.swift
+++ Sources/Todos/File.swift
@@ -13,15 +13,19 @@ struct File {
 
     init?(filePath: String) throws {
         name = filePath
-        content = try String(contentsOfFile: filePath)
-            .split(separator: "\n")
+        let fileContent = try String(contentsOfFile: filePath, encoding: .utf8)
+        let lines = fileContent.components(separatedBy: .newlines)
+
+        let lineElements = lines
             .enumerated()
             .map { Line(number: $0 + 1, content: String($1)) }
+
+        content = lineElements
     }
 }
 
 extension File: CustomDebugStringConvertible {
     var debugDescription: String {
-        return "File \(name) with \(content.count) lines"
+        return "File \(name) consisting of \(content.count) lines"
     }
 }
