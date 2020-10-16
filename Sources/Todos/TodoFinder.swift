a8e605730d1243aa48700c5f7fc4feb509fdee82
Florian Heiber <flo@flohei.de>
2020-10-16 20:12:11 +0200

    Use regex groups to cleanup lines

--- Sources/Todos/TodoFinder.swift
+++ Sources/Todos/TodoFinder.swift
@@ -8,25 +8,33 @@
 import Foundation
 
 class TodoFinder {
+    static let keywords = ["TODO", "FIXME"]
     static let keyword = "// TODO:"
 
     static func getTodoLines(in file: File) -> [Line] {
         var result = [Line]()
 
-        for line in file.content {
-            if line.qualifies(for: keyword) {
-                result.append(cleaned(line: line))
-            }
-        }
+        // Get all the lines that contain any of our keywords
+        let relevantLines = file.content.filter { $0.qualifies(for: keyword) }
+
+        // Cleanup those lines to provide for a nice print-out
+        let cleanedLines = relevantLines.map { cleaned(line: $0) }
+
+        result.append(contentsOf: cleanedLines)
 
         return result
     }
 
     static func cleaned(line: Line) -> Line {
-        // TODO: Replace this with Regex
-        let trimmed = line.content
-            .replacingOccurrences(of: keyword, with: "")
-            .trimmingCharacters(in: .whitespaces)
+        let pattern = "//\\s*(\(keywords.joined(separator: "|"))):\\s*(.*)"
+        let groups = line.content.groups(for: pattern)
+
+        // First group is either TODO or FIXME. Take second group of first match.
+        guard let trimmed = groups.first?[2] else {
+            print("Regex didn't match, returning original line")
+            return line
+        }
+
         return Line(number: line.number, content: trimmed)
     }
 }
